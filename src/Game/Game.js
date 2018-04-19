c4b1c4ffc5a4a484be9255ac95bb3ad891781146
Mark Gable <mgable43@gmail.com>
2018-04-19 16:55:37 -0700

    adding more functionality

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -5,12 +5,32 @@ import Board from './Board.js';
 class Game extends React.Component {
 	constructor(props){
 		super(props)
-		this.state = {squares: new Array(9).fill(null)}
+		this.state = {squares: new Array(9).fill(null), turn: 0}
 	}
 
 	selectSquare (idx){
-		console.info("You clicked on ", idx);
+		let squares = this.state.squares.slice(0),
+			turn = this.state.turn + 1
+		squares[idx] = this.state.turn % 2 === 0 ? "X" : "O" ;
 
+		this.setState({squares, turn});
+	}
+
+	calculateWinner(squares){
+		let lines = [ 
+			[0,1,2],
+			[3,4,5],
+			[6,7,8],
+			[0,3,6],
+			[1,4,7],
+			[2,5,8],
+			[0,4,8],
+			[2,4,6]
+		]
+
+		lines.forEach((line) => {
+			
+		});
 	}
 
 	render(){
