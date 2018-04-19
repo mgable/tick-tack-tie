34c7b9b0304a14ca2589f0b534861908a6ae84a9
Mark Gable <mgable43@gmail.com>
2018-04-19 16:55:37 -0700

    working version using state
    working
    
    polish styling

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -5,16 +5,59 @@ import Board from './Board.js';
 class Game extends React.Component {
 	constructor(props){
 		super(props)
-		this.state = {squares: new Array(9).fill(null)}
+		this.state = {squares: new Array(9).fill(null), turn: 0, winner: false, completed: false, message: "Click any square to start"}
 	}
 
 	selectSquare (idx){
-		console.info("You clicked on ", idx);
+		if (! this.state.winner && !this.state.completed){
+			let squares = this.state.squares.slice(0),
+				turn = this.state.turn + 1,
+				winner,
+				completed,
+				message;
+			
+			squares[idx] = this.state.turn % 2 === 0 ? "X" : "O";
+			winner = this.calculateWinner(squares);
+			completed = winner || (turn === squares.length);
 
+			if (winner){
+				message = `${squares[idx]} is the winner`;
+			} else if (completed){
+				message = "Tie";
+			} else {
+				message = "The game continues";
+			}
+
+			this.setState({squares, turn, winner, completed, message});
+		} else {
+			console.info("game over, man");
+		}
+	}
+
+	calculateWinner(squares){
+		let lines = [ 
+			[0,1,2],
+			[3,4,5],
+			[6,7,8],
+			[0,3,6],
+			[1,4,7],
+			[2,5,8],
+			[0,4,8],
+			[2,4,6]
+		]
+
+		for (let x = 0; x < lines.length; x++){
+			let group = lines[x];
+			if (squares[group[0]] && squares[group[1]] && squares[group[2]] && squares[group[0]] === squares[group[1]] &&  squares[group[1]] === squares[group[2]]){
+				return true
+			}
+		}
+
+		return false;
 	}
 
 	render(){
-		return (<Board squares={this.state.squares} onClick={(idx) => {this.selectSquare(idx)}}/>)
+		return (<Board message={this.state.message} squares={this.state.squares} onClick={(idx) => {this.selectSquare(idx)}}/>)
 	}
 }
 
