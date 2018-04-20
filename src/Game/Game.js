be54c7cf535801f1672cb5239c55686e98c0b17e
Mark Gable <mgable43@gmail.com>
2018-04-20 09:52:21 -0700

    working

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -5,18 +5,37 @@ import Board from './Board.js';
 class Game extends React.Component {
 	constructor(props){
 		super(props)
-		this.state = {squares: new Array(9).fill(null), turn: 0}
+		this.state = {squares: new Array(9).fill(null), turn: 0, winner: false, completed: false}
 	}
 
 	selectSquare (idx){
-		let squares = this.state.squares.slice(0),
-			turn = this.state.turn + 1
-		squares[idx] = this.state.turn % 2 === 0 ? "X" : "O" ;
+		if (! this.state.winner && !this.state.completed){
+			let squares = this.state.squares.slice(0),
+				turn = this.state.turn + 1;
+			
+			squares[idx] = this.state.turn % 2 === 0 ? "X" : "O";
+			let winner = this.calculateWinner(squares),
+				completed = winner || (turn === squares.length);
 
-		this.setState({squares, turn});
+			console.info(winner, (turn === squares.length), completed);
+
+			this.setState({squares, turn, winner, completed});
+
+			if (winner){
+				console.info("we have a winner");
+			} else if (completed){
+				console.info("tie");
+			} else {
+				console.info("the game continues");
+			}
+		} else {
+			console.info("game over, man");
+		}
 	}
 
 	calculateWinner(squares){
+		// console.info("the squares");
+		// console.info(squares);
 		let lines = [ 
 			[0,1,2],
 			[3,4,5],
@@ -28,9 +47,18 @@ class Game extends React.Component {
 			[2,4,6]
 		]
 
-		lines.forEach((line) => {
-			
-		});
+		for (let x = 0; x < lines.length; x++){
+			let group = lines[x];
+			// console.info("the group");
+			// console.info(group);
+			// console.info(squares[group[0]], squares[group[1]], squares[group[2]]);
+			if (squares[group[0]] && squares[group[1]] && squares[group[2]] && squares[group[0]] === squares[group[1]] &&  squares[group[1]] === squares[group[2]]){
+				console.info("there is a winner");
+				return true
+			}
+		}
+		console.info("no winner");
+		return false
 	}
 
 	render(){
